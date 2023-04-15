b2dc73ed1dfa23797a4131a71bc4e231c058e9b8
bruuno1994 <bruuno1015@gmail.com>
2023-04-15 13:41:56 -0300

    testes

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const app = express()
 app.use(express.json())
 
 app.listen(8080, () => {
-    console.log("Olá terráqueo! Sua porta 8080 está aberta");
+    console.log("Olá! Sua porta 8080 está aberta");
 })
 
 app.post('/includeAluno', (req, res) => {
     const { nome } = req.body
+    const { idade } = req.body
 
-    res.send(`<h1> Olá ${nome}, bem vindo!`)
+    res.send(`<h1> Olá ${nome}, seja bem vindo à sua primeira API! Você tem ${idade} anos de idade.`)
 })
