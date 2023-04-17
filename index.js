e3bf794fc9b473fff4f1238c92a72d2d96bcd7f7
bruuno1994 <bruuno1015@gmail.com>
2023-04-17 20:29:52 -0300

    Novos endpoints implementados

--- index.js
+++ index.js
@@ -5,12 +5,40 @@ const app = express()
 app.use(express.json())
 
 app.listen(8080, () => {
-    console.log("Olá! Sua porta 8080 está aberta");
+    console.log("Olá! O servidor está ativo na porta 8080");
+    console.log(Alunos);
 })
 
-app.post('/includeAluno', (req, res) => {
-    const { nome } = req.body
-    const { idade } = req.body
+let Alunos = ['Maria', 'João', 'José']
 
-    res.send(`<h1> Olá ${nome}, seja bem vindo à sua primeira API! Você tem ${idade} anos de idade.`)
+app.post('/includeAluno', (request, response) => {
+    const { nome } = request.body
+
+    Alunos.push(nome)
+
+    response.send(`<h1> Olá ${nome}, você foi inserido no Banco de Dados.`)
+})
+
+app.get('/getAluno', (request, response) => {
+    const { index } = request.body
+    // Connect SQL = SELECT * FROM Alunos WHERE id = index
+    response.send(`<h1> O aluno ${Alunos[index]} foi encontrado! </h1>`)
+})
+
+app.get('/getAlunos', (request, response) => {
+    response.send(`Todos os alunos cadastrados são: ${Alunos}`)
+})
+
+app.put('/updateAluno', (request, response) => {
+    const { index, nome } = request.body
+    Alunos[index] = nome
+    response.send('<h1> O nome foi atualizado com sucesso! </h1>');
+    console.log(Alunos);
+})
+
+app.delete('/deleteAluno', (request, response) => {
+    const { index } = request.body
+    Alunos.splice(index, 1)
+    response.send(`<h1>Alunos após o DELETE: ${Alunos}`)
+    console.log(Alunos);
 })
